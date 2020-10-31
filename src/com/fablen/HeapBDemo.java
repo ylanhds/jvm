8eb91d97a5d8bb6fc0002af80ef7141136ae9ad0
ylanhds <24424233+ylanhds@users.noreply.github.com>
2020-10-31 22:33:51 +0800

    随便写写

--- src/com/fablen/HeapBDemo.java
+++ src/com/fablen/HeapBDemo.java
@@ -13,11 +13,11 @@ package com.fablen;
 public class HeapBDemo {
     public static void main(String[] args) {
         System.out.println("Start....");
-        try {
-            Thread.sleep(1000000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Thread.sleep(1000000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         System.out.println("end....");
 
     }
